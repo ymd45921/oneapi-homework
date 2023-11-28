fe9567666d704bff1e874b87d0a5a8e0465660ce
nnm <i@shiraha.cn>
2023-11-28 08:34:22 -0800

    completed matrix multiplication.

--- src/matrix/matrix.cpp
+++ src/matrix/matrix.cpp
@@ -1,63 +1,177 @@
 #include <sycl/sycl.hpp>
 #include <iostream>
+#include <random>
+#include <limits>
 
-constexpr size_t N = 1024;  // 矩阵大小
+constexpr auto matrix_unit_size = 128;
+constexpr auto matrix_size = matrix_unit_size << 3;
+constexpr auto M = matrix_size >> 3;
+constexpr auto N = matrix_size >> 2;
+constexpr auto P = matrix_size >> 1;       
 
-class MatrixMultiplication;
+namespace my {
 
-void matrix_multiply(const std::vector<float>& matA, const std::vector<float>& matB, std::vector<float>& matC) {
+    template <typename T>
+    struct rand {
 
-    // 初始化 SYCL 环境
-    sycl::queue queue(sycl::default_selector{});
+        // 使用 C++11 的随机数引擎生成随机数
+        std::default_random_engine e;
+        std::uniform_real_distribution<T> u;
 
-    // 分配设备内存
-    sycl::buffer<float, 2> bufferA(matA.data(), sycl::range<2>(N, N));
-    sycl::buffer<float, 2> bufferB(matB.data(), sycl::range<2>(N, N));
-    sycl::buffer<float, 2> bufferC(matC.data(), sycl::range<2>(N, N));
+        rand(T min, T max) : u(min, max) {}
 
-    // 启动 kernel
-    queue.submit([&](sycl::handler& cgh) {
-        auto accessorA = bufferA.get_access<sycl::access::mode::read>(cgh);
-        auto accessorB = bufferB.get_access<sycl::access::mode::read>(cgh);
-        auto accessorC = bufferC.get_access<sycl::access::mode::write>(cgh);
+        T operator()() { return u(e); }
+    };
 
-        cgh.parallel_for<MatrixMultiplication>(sycl::range<2>(N, N), [=](sycl::item<2> item) {
-            size_t row = item.get_id(0);
-            size_t col = item.get_id(1);
+    template <typename T>
+    struct equal {
 
-            float sum = 0.0f;
-            for (size_t i = 0; i < N; ++i) {
-                sum += accessorA[{row, i}] * accessorB[{i, col}];
-            }
+        T tolerance;
 
-            accessorC[item] = sum;
-        });
-    });
+        equal(T eps = std::numeric_limits<T>::epsilon()) : tolerance(eps) {}
 
-    // 等待计算完成
-    queue.wait();
-}
+        bool operator()(T a, T b) { return std::abs(a - b) < tolerance; }
+    };
 
-int main() {
-    // 生成随机矩阵
-    std::vector<float> matA(N * N, 1.0f);
-    std::vector<float> matB(N * N, 2.0f);
-    std::vector<float> matC(N * N, 0.0f);
-
-    // 进行矩阵乘法
-    matrix_multiply(matA, matB, matC);
-
-    // 打印结果
-    #ifdef DEBUG
-        freopen("result.log", "w", stdout);
-    #endif
-    std::cout << "Result Matrix:" << std::endl;
-    for (size_t i = 0; i < N; ++i) {
-        for (size_t j = 0; j < N; ++j) {
-            std::cout << matC[i * N + j] << " ";
+    // 使用上面的随机数生成器生成 axb 的随机矩阵，a 和 b 的大小由模板参数指定
+    template <typename T, int a, int b>
+    void random_matrix(T (&matrix)[a][b], rand<T> &rand) {
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < b; j++)
+                matrix[i][j] = rand();
         }
-        std::cout << std::endl;
     }
 
+    // Host 暴力运算矩阵乘法
+    template <typename T, int A, int B, int C>
+    void matrix_multiply(const T (&a)[A][B], const T (&b)[B][C], T (&c)[A][C]) {
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < C; j++) {
+                T sum = 0;
+                for (int k = 0; k < B; k++)
+                    sum += a[i][k] * b[k][j];
+                c[i][j] = sum;
+            }
+        }
+    }
+
+    // Host 暴力运算矩阵减法
+    template <typename T, int A, int B>
+    void matrix_subtract(const T (&a)[A][B], const T (&b)[A][B], T (&c)[A][B]) {
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++)
+                c[i][j] = a[i][j] - b[i][j];
+        }
+    }
+
+    // 识别 C 二重数组的大小，并返回用来计算偏移量的函数
+    template <typename T, int A, int B>
+    constexpr auto get_offset(T (&)[A][B]) {
+        return [=](int i, int j) { return i * B + j; };
+    }
+
+    // 使用上面的浮点数 equal 函数检查两个矩阵是否相等
+    template <typename T, int A, int B>
+    bool check_matrix_equal(const T (&a)[A][B], const T (&b)[A][B], equal<T> &eq) {
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++)
+                if (!eq(a[i][j], b[i][j]))
+                    return false;
+        }
+        return true;
+    }
+
+    // 将矩阵输出到文件
+    template <typename T, int A, int B>
+    void print_matrix(std::ostream &os, const T (&matrix)[A][B]) {
+        for (int i = 0; i < A; i++) {
+            for (int j = 0; j < B; j++)
+                os << matrix[i][j] << " ";
+            os << "\n";
+        }
+    }
+}
+ 
+signed main() {
+
+    float a_host[M][N], b_host[N][P], c_host[M][P];
+    my::rand<float> rand_float(0, 1);
+    random_matrix(a_host, rand_float);
+    random_matrix(b_host, rand_float);
+
+    float (*c_out)[P] = new float[M][P], c_dev[M][P];
+
+    try {
+        sycl::queue q(sycl::default_selector_v);
+        std::cout << "Running on device: "
+                  << q.get_device().get_info<sycl::info::device::name>() << "\n";
+
+        sycl::buffer<float, 2> a_buf(sycl::range(M, N));
+        sycl::buffer<float, 2> b_buf(sycl::range(N, P));
+        sycl::buffer c_buf((float *)c_out, sycl::range(M, P));
+        std::cout << "Problem size: " << "c[" << M << "][" << P << "] = a[" 
+                  << M << "][" << N << "] * b[" << N << "][" << P << "]\n";
+
+        q.submit([&](sycl::handler &h) {
+            // 获得了对 Device 上的 a_buf 的访问权
+            sycl::accessor a(a_buf, h, sycl::write_only);
+            auto p_a_host = (float *)a_host;
+            auto offset = my::get_offset(a_host);
+            h.parallel_for(sycl::range(M, N), [=](sycl::id<2> index) {
+                a[index] = p_a_host[offset(index[0], index[1])];
+            });
+        });
+        q.submit([&](sycl::handler &h) {
+            // 获得了对 Device 上的 b_buf 的访问权
+            sycl::accessor b(b_buf, h, sycl::write_only);
+            auto p_b_host = (float *)b_host;
+            auto offset = my::get_offset(b_host);
+            h.parallel_for(sycl::range(N, P), [=](sycl::id<2> index) {
+                b[index] = p_b_host[offset(index[0], index[1])];
+            });
+        });
+
+        q.submit([&](sycl::handler &h) {
+            sycl::accessor a(a_buf, h, sycl::read_only);
+            sycl::accessor b(b_buf, h, sycl::read_only);
+            sycl::accessor c(c_buf, h, sycl::write_only, sycl::no_init);
+
+            int a_width = a.get_range()[1];
+
+            h.parallel_for(sycl::range(M, P), [=](sycl::id<2> index) {
+                float sum = 0;
+                auto row = index[0], col = index[1];
+                for (int i = 0; i < a_width; i++) {
+                    sum += a[row][i] * b[i][col];
+                }
+                c[index] = sum;
+            });
+        });
+    } catch (sycl::exception const &e) {
+        std::cout << "An exception is caught for matrix multiplication.\n";
+        std::terminate();
+    }
+
+    std::cout << "Running on host...\n";
+    my::matrix_multiply(a_host, b_host, c_host);
+    std::memcpy(c_dev, c_out, sizeof(c_dev));
+
+    my::equal eq(1e-4f);
+    if (my::check_matrix_equal(c_host, c_dev, eq)) {
+        std::cout << "Matrix multiplication successfully completed on device.\n";
+    } else {
+        std::cout << "Matrix multiplication failed on device.\n";
+        std::ofstream ofs("matrix.txt");
+        ofs << "Device output:\n";
+        my::print_matrix(ofs, c_dev);
+        ofs << "Host output:\n";
+        my::print_matrix(ofs, c_host);
+        float c_diff[M][P];
+        my::matrix_subtract(c_host, c_dev, c_diff);
+        ofs << "Difference:\n";
+        my::print_matrix(ofs, c_diff);
+        ofs.close();
+    }
+    delete[] c_out;
     return 0;
 }
